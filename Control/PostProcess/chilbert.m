650cadf1e0c3fd1d5f3e79b725263c4b843497e6
Eduardo Martini <eduardo.martini@univ-poitiers.fr>
2020-11-27 08:47:25 +0100

    Cleanup chilbert.m

--- Control/PostProcess/chilbert.m
+++ Control/PostProcess/chilbert.m
@@ -12,24 +12,12 @@ function  y =  chilbert(x,overSample)
     n=length(x);
     y=zeros(size(x));
 
-    if false % tailored method (not working)
-        signw= [0;ones(n*overSample/2-1,1);0;-ones(n*overSample/2-1,1)];
-        signw = reshape(signw,1,1,n*overSample);
-        for i=1:size(x,1)
-            for j=1:size(x,2)
-                ytmp =ifft(fft(x(i,j,:),n*overSample,3).*(signw),n,3);
-                y(i,j,:)=reshape(ytmp,1,1,n);
-            end
+    for i=1:size(x,1)
+        for j=1:size(x,2)
+            xr= squeeze(real(x(i,j,:)));
+            xi= squeeze(imag(x(i,j,:)));
+            ytmp  = imag(hilbert(xr,n*overSample))+1i*imag(hilbert(xi,n*overSample));
+            y(i,j,:) = ytmp(1:n);
         end
-    else % using matlab function)
-    %     disp(['Oversample ' num2str(overSample)]);
-        for i=1:size(x,1)
-            for j=1:size(x,2)
-                xr= squeeze(real(x(i,j,:)));
-                xi= squeeze(imag(x(i,j,:)));
-                ytmp  = imag(hilbert(xr,n*overSample))+1i*imag(hilbert(xi,n*overSample));
-                y(i,j,:) = ytmp(1:n);
-            end
-        end
-end
+    end
     
